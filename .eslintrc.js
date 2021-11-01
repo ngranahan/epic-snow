bcdf9b47d1d8ece9f5b2f6340b2b66014f15bc99
Nora Granahan <namgranahan@gmail.com>
2021-10-31 22:16:00 -0400

    Initial setup from vue cli with prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,13 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        trailingComma: 'none'
+      }
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
